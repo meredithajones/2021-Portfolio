04ebbdda1db9fc7a966a62195ae5d30033af8c3b
Meredith Jones <mj19003@gmail.com>
2021-02-27 10:05:02 -0500

    Outlining app.js page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state ={
-      title:'Meredith Jones', 
+    this.state = {
+      title: "Meredith Jones",
       headerLinks: [
-        { title: 'Home', path: '/'},
-        { title: 'About', path: ''},
-        { title: 'Contact', path: '/'}
-      ]
-    }
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
+      ],
+      home: {
+        title: "Creating Beautiful Online Spaces",
+        subTitle: "Coding for the greater good",
+        greeting: "Take a look at a few of my projects below",
+      },
+      about: {
+        title: "About Me",
+      },
+      contact: {
+        title: "Get In Touch",
+      },
+    };
   }
-
   render() {
-  return (
-    <div>
-      
-      
-    </div>
-    );
+    return <div>Projects</div>;
   }
 }
 
