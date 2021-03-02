d0d840e836f374e51b13f69a9c439807bd851893
Meredith Jones <mj19003@gmail.com>
2021-03-02 07:40:21 -0500

    Finished Navbar content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FaReact  } from 'react-icons/fa'; 
+
 
 
 function Footer() {
 
     return(
         <footer className= "mt-5">
-            <Container>
-                <Row>
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
                     <Col className='p-0' md={3} sm={12} >
                     Meredith Jones 2021
+                    </Col>
+                    <Col className="p-0" d-flex justify-content-end md={3}>
+                    Site created using React <FaReact />
                     </Col>
 
                 </Row>
