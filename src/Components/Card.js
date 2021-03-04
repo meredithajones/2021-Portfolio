e0ea304ffda6e87192bb0dc32c4d883421908050
Meredith Jones <mj19003@gmail.com>
2021-03-04 14:50:40 -0500

    Set up useSpring for animations in cardinfo component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import CardInfo from './CardInfo';
+
 function Card(props) {
 
     return(
         <div>
             <img className="" src={props.item.imgSrc} alt= {props.item.imgSrc}/>
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
-          ;  </div>
+            </div>
     )
 }
 export default Card;
--- src/Components/CardInfo.js
+++ src/Components/CardInfo.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {useSpring, animated } from 'react-spring';
 
 function CardInfo(props) {
 
+    const style = useSpring({opacity: 1, from: { opacity:0 }});
+
     return(
-        <p>Card Info Works!</p>
-    )
+        <animated.div className="" style={style}>
+            <p>{props.title}</p>
+            <p>{props.subtitle}</p>
+            <a href={props.link} target= "_blank" rel="noopener noreferrer">View</a>
+
+            </animated.div>
+    );
     
 }
 
