953564e9b60c05b3629ccbe0bca0b0fb04d932c2
Meredith Jones <mj19003@gmail.com>
2021-03-04 14:29:20 -0500

    Added content to card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,8 @@ function Card(props) {
 
     return(
         <div>
-            <img className="" src={props.item.imgSrc} />
+            <img className="" src={props.item.imgSrc} alt= {props.item.imgSrc}/>
+            { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} /> }
           ;  </div>
     )
 }
