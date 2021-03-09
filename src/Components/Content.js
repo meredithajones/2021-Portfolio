9b811f0c5d7eb58aedbe7b5a7007294b2c8bba55
Meredith Jones <mj19003@gmail.com>
2021-03-09 16:02:15 -0500

    Created and set up framework from content.js component

---
+++ src/Components/Content.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+function Content(props) {
+
+    return(
+        <Container>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+        </Container>
+
+    );
+}
+
+export default Content;
