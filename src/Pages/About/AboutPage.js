d48ab112a64a8d9104f2104f1fa2b4b9bd95c537
Meredith Jones <mj19003@gmail.com>
2021-03-10 13:36:51 -0500

    Added skills section to bio

--- src/Pages/About/AboutPage.js
+++ src/Pages/About/AboutPage.js
@@ -18,6 +18,16 @@ function AboutPage(props) {
             <p>My background includes an intensive full-stack coding education through 
                 the University of Pennsylvania's college of Liberal and Professional Studies.
             </p>
+            <p>Skills include:</p>
+            <li>Javascript</li> 
+            <li>jQuery</li> 
+            <li>Bootstap</li>
+            <li> Mongoose</li> 
+            <li>MongoDB</li> 
+            <li>Node.JS</li> 
+            <li>Handlebars</li> 
+            <li>Express</li> 
+            <li>React</li>
             </div>
         </Content>
        </div>
