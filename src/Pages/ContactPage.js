5a8d1d624dae88c8d62e23c6e383d9ae7257ec85
Meredith Jones <mj19003@gmail.com>
2021-03-12 15:24:28 -0500

    Started building out contact form

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../Components/Hero';
 import Content from '../Components/Content';
+import Form from 'react-bootstrap/Form';
 
 
 class ContactPage extends React.Component {
@@ -22,7 +23,16 @@ class ContactPage extends React.Component {
             <Hero title={this.props.title} />
 
             <Content>
+                <Form>
+                    <Form.Group>
+                        <Form.Label htmlFor="full-name">
+                            Full Name
+                        </Form.Label>
+                        <Form.Control id="full-name" name="name" type="text" value={this.state.name} />
+                    </Form.Group>
 
+
+                </Form>
             </Content>
         </div>
         );
