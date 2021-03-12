7244ed3106e1881ef86340f94fa68bfd244496d2
Meredith Jones <mj19003@gmail.com>
2021-03-12 15:11:07 -0500

    Fixed import issue in contact page

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,24 +1,32 @@
-import { React } from 'react';
+import React from 'react';
+import Hero from '../Components/Hero';
+import Content from '../Components/Content';
+
 
 class ContactPage extends React.Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             name: '',
             email: '',
             message: '',
             disabled: false,
             emailSent: null,
-
         }
     }
+    
 
     render() {
         return(
-        <p>ContactPage Works!</p>
-    );
-}
+        <div>
+            <Hero title={this.props.title} />
+
+            <Content>
+
+            </Content>
+        </div>
+        );
+    }
 }
 
 export default ContactPage;
