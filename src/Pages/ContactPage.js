abe2e7421bf9eb8f295afa1f98f8f57a5beffd85
Meredith Jones <mj19003@gmail.com>
2021-03-12 15:28:52 -0500

    Added email and message fields to contact component

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -28,7 +28,19 @@ class ContactPage extends React.Component {
                         <Form.Label htmlFor="full-name">
                             Full Name
                         </Form.Label>
-                        <Form.Control id="full-name" name="name" type="text" value={this.state.name} />
+                        <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange}/>
+
+                        <Form.Group>
+                            <Form.Label htmlFor="email">Email</Form.Label>
+                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+                        </Form.Group>
+
+
+                        <Form.Group>
+                            <Form.Label htmlFor="message">Message</Form.Label>
+                            <Form.Control id="message" name="message" as="textarea"rows="3" value={this.state.message} onChange={this.handleChange} />
+                        </Form.Group>
+                        
                     </Form.Group>
 
 
