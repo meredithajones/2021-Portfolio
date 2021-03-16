5b6e865d2ead42dfea3da67f256c27d8eb639cce
Meredith Jones <mj19003@gmail.com>
2021-03-16 15:51:48 -0400

    Pivoting to setup for emailjs instead of mailgrid

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -5,107 +5,33 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import Axios from 'axios';
+import emailjs from 'emailjs-com';
 
+export default function ContactUs() {
 
-class ContactPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
-            disabled: false,
-            emailSent: null,
-        }
-    }
-    //Creating functions to handle events
-    handleChange = (event) => {
-        console.log(event);
+        function sendEmail(e) {
+            e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'youtube_template', e.target, 'user_rMuPcHScdQ7joaXqMmqZr')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
 
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+}
 
-        this.setState({
-            [name] : value
-        })
-    }
-    
-    //Setting up prevent default to stop page refresh on submit
-        handleSubmit = (event) => {
-            event.preventDefault();
-
-            //Preventing multiple email submissions
-            this.setState({
-                disabled: true,
-                emailSent: true,
-            });
-
-            Axios.post('http://localhost:3030/api/email', this.state)
-            .then(res => {
-                if(res.data.success) {
-                    this.setState({
-                        disabled: false,
-                        emailSent: true
-                    });
-                } else {
-                    this.setState({
-                        disabled: false,
-                        emailSent: false
-                    });
-                }
-            })
-            .catch(err => {
-                console.log(err);
-
-                this.setState({
-                    disabled: false,
-                    emailSent: false
-                });
-            })
-
-    }
-    
-
-        render() {
-            return(
-                <div>
-                    <Hero title={this.props.title} />
-    
-                    <Content>
-                        <Form onSubmit={this.handleSubmit}>
-                            <Form.Group>
-                                <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                                <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
-                            </Form.Group>
-    
-    
-                            <Form.Group>
-                                <Form.Label htmlFor="email">Email</Form.Label>
-                                <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
-                            </Form.Group>
-    
-    
-                            <Form.Group>
-                                <Form.Label htmlFor="message">Message</Form.Label>
-                                <Form.Control id="message" name="message" as="textarea"rows="3" value={this.state.message} onChange={this.handleChange} />
-                            </Form.Group>
-    
-    
-                            <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
-                                Send
-                            </Button>
-    
-    
-                            {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
-                            {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
-                        </Form>
-                    </Content>
-                </div>
-            );
-        }
-    
-    }
-    
-    export default ContactPage;
     
