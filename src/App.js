19f196e566f45a066386eb77de9a1181c6083652
Meredith Jones <mj19003@gmail.com>
2021-03-18 10:20:02 -0400

    Troubleshooting page routes

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar.toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/home">
+                {/* <Link className="nav-link" to="/home">
                   Home
                 </Link>
                 <Link className="nav-link" to="/about">
@@ -63,7 +63,7 @@ class App extends React.Component {
                 </Link>
                 <Link className="nav-link" to="/contact">
                   Contact
-                </Link>
+                </Link> */}
               </Nav>
             </Navbar.Collapse>
           </Navbar>
