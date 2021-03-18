39278cc5d2ff2acf2b0233915cdec2c4399d77e6
Meredith Jones <mj19003@gmail.com>
2021-03-18 10:07:57 -0400

    Fixes to landing page address

--- src/App.js
+++ src/App.js
@@ -69,10 +69,9 @@ class App extends React.Component {
           </Navbar>
           <Sidenav />
           
-
-          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+          <Route exact path="/" render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route exact path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route exact path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
           <Footer />
         </Container>
       </Router>
